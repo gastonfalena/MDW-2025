c4304984470b44ea68855e0c60f4131fe12d6d00
gastonfalena <gastonfalena@gmail.com>
2025-08-19 00:24:11 -0300

    add: NaN validation and support for multiple input

--- stdin/index.js
+++ stdin/index.js
@@ -1,12 +1,17 @@
 const pkg = require("./package.json");
 
 process.stdin.on("data", (data) => {
-  const [a, b] = data
+  const numbers = data
     .toString()
     .trim()
     .split("\n")
-    .map((data) => Number(data));
-  console.log(a + b);
+    .map((data) => {
+      const num = Number(data);
+      return isNaN(num) ? 0 : num;
+    });
+
+  const result = numbers.reduce((total, num) => total + num, 0);
+  console.log(result);
 });
 
 // Get-Content input.txt | node index.js | Out-File salida.txt
